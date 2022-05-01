6d5bc7c26afb4712e4bb7c4276f8bab875d5ee1b
Darijo <darijoorlic@gmail.com>
2022-05-01 11:37:29 +0200

    V1-knjige-objekat-modul

---
+++ Knjige_objekat_modul/index.js
@@ -0,0 +1,30 @@
+let knjige = [
+    
+    {
+        "id": 1,
+        "naziv": "Numerička matematika",
+        "autor": "Nataša Ćirović"
+    },
+    {
+        "id": 2,
+        "naziv": "Računarske mreže 2",
+        "autor": "Pavle Vuletić"
+    },
+    {
+        "id": 3,
+        "naziv": "Programski jezik C#",
+        "autor": "Laslo Kraus"
+    },
+    {
+        "id": 4,
+        "naziv": "Tehnike Vizuelnog Programiranja",
+        "autor": "Zoran Cirovic"
+    },
+    {
+        "id": 5,
+        "naziv": "Objektno Orijentisano Programiranje",
+        "autor": "Perica Strbac"
+    }
+];
+
+exports.listaknjiga = knjige;
