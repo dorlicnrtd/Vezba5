155548d05c8aa8cef3853647ef676dd2410c1494
Darijo <darijoorlic@gmail.com>
2022-05-01 14:31:30 +0200

    V3-dodat test i promenjena verzija za knjige_metode_modul

---
+++ Testiranje/index.js
@@ -0,0 +1,15 @@
+const test = require('@dorlicnrtd/knjige_metode_modul');
+
+console.log(test.sveKnjige());
+// console.log(test.deleteKnjiga(2));
+// console.log(test.sveKnjige());
+
+// test.addKnjiga(4,"Programiranje","Petar Petrovic");
+// console.log(test.sveKnjige());
+
+// console.log(test.getKnjigaByAuthor("Perica Strbac"));
+
+// test.postaviAutora("Programski jezik C#","Nikola Nikolic");
+// console.log(test.sveKnjige());
+
+//console.log(test.getKnjiga(2));
